700b32501c74e845533ccd51ffc1ce257b7a192c
Danny Roberts <danny@thefallenphoenix.net>
2015-02-16 22:52:11 +0000

    Updated define for importing gpg keys

--- manifests/rpm_gpg_key.pp
+++ manifests/rpm_gpg_key.pp
@@ -1,4 +1,4 @@
-define ius::rpm_gpg_key($path) {
+define elrepo::rpm_gpg_key($path) {
   # Given the path to a key, see if it is imported, if not, import it
   exec { "import-${name}":
     path 	=> '/bin:/usr/bin:/sbin:/usr/sbin',
