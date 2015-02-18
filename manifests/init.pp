1ba439e38e5eeee2abbb1d898b220448300e1023
Danny Roberts <danny.roberts@reconnix.com>
2015-02-18 08:24:01 +0000

    Added remaining repos

--- manifests/init.pp
+++ manifests/init.pp
@@ -12,12 +12,33 @@
 #
 class elrepo (
 
-  $elrepo_baseurl    = $::elrepo::params::elrepo_baseurl,
-  $elrepo_mirrorlist = $::elrepo::params::elrepo_mirrorlist,
-  $elrepo_proxy      = $::elrepo::params::elrepo_proxy,
-  $elrepo_enabled    = $::elrepo::params::elrepo_enabled,
-  $elrepo_gpgcheck   = $::elrepo::params::elrepo_gpgcheck,
-  $elrepo_gpgcheck   = $::elrepo::params::elrepo_protect,
+  $elrepo_baseurl            = $::elrepo::params::elrepo_baseurl,
+  $elrepo_mirrorlist         = $::elrepo::params::elrepo_mirrorlist,
+  $elrepo_proxy              = $::elrepo::params::elrepo_proxy,
+  $elrepo_enabled            = $::elrepo::params::elrepo_enabled,
+  $elrepo_gpgcheck           = $::elrepo::params::elrepo_gpgcheck,
+  $elrepo_protect            = $::elrepo::params::elrepo_protect,
+
+  $elrepo_testing_baseurl    = $::elrepo::params::elrepo_testing_baseurl,
+  $elrepo_testing_mirrorlist = $::elrepo::params::elrepo_testing_mirrorlist,
+  $elrepo_testing_proxy      = $::elrepo::params::elrepo_testing_proxy,
+  $elrepo_testing_enabled    = $::elrepo::params::elrepo_testing_enabled,
+  $elrepo_testing_gpgcheck   = $::elrepo::params::elrepo_testing_gpgcheck,
+  $elrepo_testing_protect    = $::elrepo::params::elrepo_testing_protect,
+
+  $elrepo_kernel_baseurl     = $::elrepo::params::elrepo_kernel_baseurl,
+  $elrepo_kernel_mirrorlist  = $::elrepo::params::elrepo_kernel_mirrorlist,
+  $elrepo_kernel_proxy       = $::elrepo::params::elrepo_kernel_proxy,
+  $elrepo_kernel_enabled     = $::elrepo::params::elrepo_kernel_enabled,
+  $elrepo_kernel_gpgcheck    = $::elrepo::params::elrepo_kernel_gpgcheck,
+  $elrepo_kernel_protect     = $::elrepo::params::elrepo_kernel_protect,
+
+  $elrepo_extras_baseurl     = $::elrepo::params::elrepo_extras_baseurl,
+  $elrepo_extras_mirrorlist  = $::elrepo::params::elrepo_extras_mirrorlist,
+  $elrepo_extras_proxy       = $::elrepo::params::elrepo_extras_proxy,
+  $elrepo_extras_enabled     = $::elrepo::params::elrepo_extras_enabled,
+  $elrepo_extras_gpgcheck    = $::elrepo::params::elrepo_extras_gpgcheck,
+  $elrepo_extras_protect     = $::elrepo::params::elrepo_extras_protect,
 
 ) inherits elrepo::params {
 
@@ -34,6 +55,39 @@ class elrepo (
       protect    => $elrepo_protect,
     }
 
+    yumrepo { 'elrepo-testing':
+      descr      => "ELRepo.org Community Enterprise Linux Testing Repository - el${::operatingsystemmajrelease}",
+      baseurl    => $elrepo_testing_baseurl,
+      mirrorlist => $elrepo_testing_mirrorlist,
+      proxy      => $elrepo_testing_proxy,
+      enabled    => $elrepo_testing_enabled,
+      gpgcheck   => $elrepo_testing_gpgcheck,
+      gpgkey     => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-elrepo.org',
+      protect    => $elrepo_testing_protect,
+    }
+
+    yumrepo { 'elrepo-kernel':
+      descr      => "ELRepo.org Community Enterprise Linux Kernel Repository - el${::operatingsystemmajrelease}",
+      baseurl    => $elrepo_kernel_baseurl,
+      mirrorlist => $elrepo_kernel_mirrorlist,
+      proxy      => $elrepo_kernel_proxy,
+      enabled    => $elrepo_kernel_enabled,
+      gpgcheck   => $elrepo_kernel_gpgcheck,
+      gpgkey     => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-elrepo.org',
+      protect    => $elrepo_kernel_protect,
+    }
+
+    yumrepo { 'elrepo-extras':
+      descr      => "ELRepo.org Community Enterprise Linux Extras Repository - el${::operatingsystemmajrelease}",
+      baseurl    => $elrepo_extras_baseurl,
+      mirrorlist => $elrepo_extras_mirrorlist,
+      proxy      => $elrepo_extras_proxy,
+      enabled    => $elrepo_extras_enabled,
+      gpgcheck   => $elrepo_extras_gpgcheck,
+      gpgkey     => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-elrepo.org',
+      protect    => $elrepo_extras_protect,
+    }
+
     file { '/etc/pki/rpm-gpg/RPM-GPG-KEY-elrepo.org':
       ensure => 'present',
       owner  => 'root',
@@ -42,10 +96,10 @@ class elrepo (
       source => 'puppet:///modules/elrepo/RPM-GPG-KEY-elrepo.org',
     }
 
-    ius::rpm_gpg_key{ 'RPM-GPG-KEY-elrepo.org':
+    elrepo::rpm_gpg_key{ 'RPM-GPG-KEY-elrepo.org':
       path    => '/etc/pki/rpm-gpg/RPM-GPG-KEY-elrepo.org',
       require => File['/etc/pki/rpm-gpg/RPM-GPG-KEY-elrepo.org'],
-      before  => Yumrepo['elrepo'],
+      before  => Yumrepo['elrepo', 'elrepo-testing', 'elrepo-kernel', 'elrepo-extras'],
     }
   } else {
     notice ("Your operating system ${::operatingsystem} will not have the ELRepo repository applied")
--- manifests/params.pp
+++ manifests/params.pp
@@ -7,11 +7,32 @@ class elrepo::params {
   # the most specific declaration of proxy.
   $proxy             = 'absent'
 
-  $elrepo_baseurl    = 'absent'
-  $elrepo_mirrorlist = "http://mirrors.elrepo.org/mirrors-elrepo.el${::operatingsystemmajrelease}"
-  $elrepo_proxy      = $proxy
-  $elrepo_enabled    = '1'
-  $elrepo_gpgcheck   = '1'
-  $elrepo_protect    = '0'
+  $elrepo_baseurl            = 'absent'
+  $elrepo_mirrorlist         = "http://mirrors.elrepo.org/mirrors-elrepo.el${::operatingsystemmajrelease}"
+  $elrepo_proxy              = $proxy
+  $elrepo_enabled            = '1'
+  $elrepo_gpgcheck           = '1'
+  $elrepo_protect            = '0'
+
+  $elrepo_testing_baseurl    = 'absent'
+  $elrepo_testing_mirrorlist = "http://mirrors.elrepo.org/mirrors-elrepo-testing.el${::operatingsystemmajrelease}"
+  $elrepo_testing_proxy      = $proxy
+  $elrepo_testing_enabled    = '0'
+  $elrepo_testing_gpgcheck   = '1'
+  $elrepo_testing_protect    = '0'
+
+  $elrepo_kernel_baseurl     = 'absent'
+  $elrepo_kernel_mirrorlist  = "http://mirrors.elrepo.org/mirrors-elrepo-kernel.el${::operatingsystemmajrelease}"
+  $elrepo_kernel_proxy       = $proxy
+  $elrepo_kernel_enabled     = '0'
+  $elrepo_kernel_gpgcheck    = '1'
+  $elrepo_kernel_protect     = '0'
+
+  $elrepo_extras_baseurl     = 'absent'
+  $elrepo_extras_mirrorlist  = "http://mirrors.elrepo.org/mirrors-elrepo-extras.el${::operatingsystemmajrelease}"
+  $elrepo_extras_proxy       = $proxy
+  $elrepo_extras_enabled     = '0'
+  $elrepo_extras_gpgcheck    = '1'
+  $elrepo_extras_protect     = '0'
 
 }
